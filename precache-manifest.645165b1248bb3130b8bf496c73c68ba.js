c8056377f3fb09dd31e0c5fb9b185e6468974317
Katelyn Schmidt <17schmidtkatelyn@berkeley.edu>
2019-10-13 11:04:00 -0700

    Updates

---
+++ precache-manifest.645165b1248bb3130b8bf496c73c68ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397b9403306c19013bb567527e50be9a",
+    "url": "/api_project/index.html"
+  },
+  {
+    "revision": "344274d333dff95dc83b",
+    "url": "/api_project/static/css/main.da4d91d8.chunk.css"
+  },
+  {
+    "revision": "cd45fb9ead75aa14d187",
+    "url": "/api_project/static/js/2.2f2993f9.chunk.js"
+  },
+  {
+    "revision": "344274d333dff95dc83b",
+    "url": "/api_project/static/js/main.da8c05b3.chunk.js"
+  },
+  {
+    "revision": "789f243acad06ea85a98",
+    "url": "/api_project/static/js/runtime-main.87057efa.js"
+  },
+  {
+    "revision": "ee7491da7fe17e8f22980fa14b64ef25",
+    "url": "/api_project/static/media/yoda.ee7491da.png"
+  }
+]);
